09e67ef76579fed9e79fe75e42837b68467213e4
Carlos Miguel <85066001+ckzwebber@users.noreply.github.com>
2024-08-07 11:14:11 -0300

    Work

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let timerHours = document.getElementById("timer-hours");
 let timerMinutes = document.getElementById("timer-minutes");
 let timerSeconds = document.getElementById("timer-seconds");
+let startButton = document.getElementById("start-button");
+let resetButton = document.getElementById("reset-button");
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let timer;
 let timers = [timerHours, timerMinutes, timerSeconds];
 
 editTimer();
@@ -27,49 +33,72 @@ function editingTimer(element) {
     input.type = "number";
     input.value = numberOfInput;
     input.className = "timer-input";
-    input.min = "0";
-
-    switch (element.id) {
-        case "timer-hours":
-            input.max = "150";
-            break;
-        case "timer-minutes":
-            input.max = "59";
-            break;
-        case "timer-seconds":
-            input.max = "59";
-    }
-
 
     input.addEventListener("blur", () => {
         let inputInt = parseInt(input.value);
-        if (inputInt > 99) {
-            input.value = "99";
-        } else if (inputInt < 0) {
-            input.value = "0";
+        switch (element.id) {
+
+            case "timer-hours":
+                inputInt > 99 ? input.value = "99" : input.value;
+                inputInt < 0 ? input.value = "00" : input.value;
+                element.textContent = `${input.value}h`;
+                hours = parseInt(input.value);
+                break;
+
+            case "timer-minutes":
+                inputInt > 59 ? input.value = "59" : input.value;
+                inputInt < 0 ? input.value = "00" : input.value;
+                element.textContent = `${input.value}m`;
+                minutes = parseInt(input.value);
+                break;
+
+            case "timer-seconds":
+                inputInt > 59 ? input.value = "59" : input.value;
+                inputInt < 0 ? input.value = "00" : input.value;
+                element.textContent = `${input.value}s`;
+                seconds = parseInt(input.value);
+                break;
         }
-        save(element, input, numberOfInput);
+        editTimer(element);
     })
 }
 
-function save(element, input, numberOfInput) {
-    let newTimer = input.value.trim();
+startButton.addEventListener("click", () => start(hours, minutes, seconds));
+resetButton.addEventListener("click", reset);
 
-    console.log(element);
-    if (newTimer !== isNaN && newTimer !== "") {
-        if (element.id === "timer-hours") {
-            element.textContent = `${newTimer}h`;
-        } else if (element.id === "timer-minutes") {
-            element.textContent = `${newTimer}m`;
-        } else if (element.id === "timer-seconds") {
-            element.textContent = `${newTimer}s`;
-        } else {
-            element.textContent = `${numberOfInput}`;
-        }
-    }
 
-    editTimer(element);
+function start(hours, minutes, seconds) {
+    console.log("Depois do start: " + hours, minutes, seconds);
+    // stop();
+    // timer = setInterval(() => {
+    //     startingTimer();
+    // }, 1000);
 }
 
+function startingTimer() {
 
+}
 
+function stop() {
+    clearInterval(timer);
+}
+
+function reset() {
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    timers.forEach(element => {
+        if (element.id === "timer-hours") {
+
+            element.textContent = "00h";
+
+        } else if (element.id === "timer-minutes") {
+
+            element.textContent = "00m";
+
+        } else if (element.id === "timer-seconds") {
+
+            element.textContent = "00s";
+        }
+    })
+}
